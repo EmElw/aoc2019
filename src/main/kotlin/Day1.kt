0e3341dea6deb1e65e11c0b802d87cbbf90af0d3
Magnus Wamby <wamby@student.chalmers.se>
2019-12-02 20:48:37 +0100

    file cleanup

--- src/main/kotlin/Day1.kt
+++ src/main/kotlin/Day1.kt
@@ -9,9 +9,9 @@ fun calcFuel(v: Int): Int = v / 3 - 2
 
 fun sumFuel(modules: List<Int>): Int = modules.map { calcFuel(it) }.sum()
 
-fun calcFuelRec(v: Int): Int {
-    val f = calcFuel(v)
-    return if (f > 0) f + calcFuelRec(f) else 0
+fun calcFuelRec(mass: Int): Int {
+    val fuel = calcFuel(mass)
+    return if (fuel > 0) fuel + calcFuelRec(fuel) else 0
 }
 
 fun sumFuelRec(modules: List<Int>): Int = modules.map { calcFuelRec(it) }.sum()
