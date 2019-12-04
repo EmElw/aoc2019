af46c85815fbbe04cd2a78f08dba353f090f0f03
Magnus Wamby <wamby@student.chalmers.se>
2019-12-04 12:00:46 +0100

    fix input formatting for day4

--- src/main/kotlin/Day4.kt
+++ src/main/kotlin/Day4.kt
@@ -1,17 +1,17 @@
 fun main() {
-    val (lo, hi) = getInput(4).split("-").map { it.toInt() }
+    val (lo, hi) = getInput(4).split("-").map { it.trim().toInt() }
     println("4.1:${(lo..hi).map { it.toString() }.filter { criteria(it) }.size}")
     println("4.2:${(lo..hi).map { it.toString() }.filter { criteria2(it) }.size}")
 }
 
 fun criteria(s: String): Boolean = all(
-    (s.zipWithNext().none { (a, b) -> b < a }),
-    (s.zipWithNext().any { (a, b) -> a == b })
+    s.zipWithNext().none { (a, b) -> b < a },
+    s.zipWithNext().any { (a, b) -> a == b }
 )
 
 fun criteria2(s: String): Boolean = all(
-    (s.zipWithNext().none { (a, b) -> b < a }),
-    (('0'..'9').any { s.indexOfLast { c -> c == it } - s.indexOfFirst { c -> c == it } == 1 })
+    s.zipWithNext().none { (a, b) -> b < a },
+    ('0'..'9').any { s.indexOfLast { c -> c == it } - s.indexOfFirst { c -> c == it } == 1 }
 )
 
 fun all(vararg conditions: Boolean) = conditions.all { it }
